82dd5b8d0e7f5638a9831ec6338d3d2575bfac16
chikechris <cchike2017@gmail.com>
2020-06-17 12:05:21 -0400

    pokemon api works

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -2,22 +2,28 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function Pokemon () {
-  const [numb, setNumb] = useState()
+  const [numb, setNumb] = useState() 
+  const [name, setName] = useState() 
+  const [height, setHeight] = useState() 
+  const [moves, setMoves] = useState()
 
   useEffect(() => {
     async function getPokemon () {
       const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${numb}`)
-      console.log(res.data.name)
+      console.log(res.data) 
+      setName(res.data.name)
+      setHeight(res.data.height)
+      setMoves(res.data.moves.length)
+
     }
     getPokemon()
   })
 
   return (
     <div>
-      <h1>I am Pokemon number <span style={{ color: 'blue'}}> {numb}</span>. </h1>
-      <h1>My name is {name}. </h1>
-      <h1>My height is {height}inches </h1>
-      <h1>I have {moves} moves. </h1>
+      Choose Your Pokemon
+      <br />
+   
       <select
         value={numb}
         onChange={e => {
@@ -31,6 +37,12 @@ function Pokemon () {
         <option value='7'> 7 </option>
         <option value='8'> 8 </option>
       </select>
+
+      <h1>I am Pokemon number <span style={{ color: 'blue' }}> {numb}</span>. </h1>
+      <h1>My name is {name}. </h1>
+      <h1>My height is {height} inches </h1>
+      <h1>I have {moves} moves. </h1>
+
     </div>
   )
 }
